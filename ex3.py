f3505e28feadcf7957bb6ebfe880c22c20f4135b
Cameryn Rhosyn <camerynrhosyn@gmail.com>
2017-10-31 13:45:17 -0400

    LPTHW ex3

---
+++ ex3.py
@@ -0,0 +1,35 @@
+# prints phrase
+print "I will now count my chickens:"
+
+# prints phrase and does math
+print "Hens", 25 + 30 / 6
+# prints phrase and does math
+print "Roosters", 100 - 25 * 3 % 4
+
+# prints phrase
+print "Now I will count the eggs:"
+
+# prints math
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# prints phrase
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# prints math
+print 3 + 2 < 5 - 7
+
+# prints phrase and math
+print "What is 3 + 2?", 3 + 2
+# prints phrase and math
+print "What is 5 - 7?", 5 - 7
+
+# prints phrase
+print "Oh, that's why it's False."
+
+# prints phrase
+print "How about some more."
+
+# prints phrase and math
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
