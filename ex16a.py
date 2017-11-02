0aaf023c19b93e874cf3906686ed23f0937e8c67
Cameryn Rhosyn <camerynrhosyn@gmail.com>
2017-11-02 18:32:30 -0400

    LPTHW

--- ex16a.py
+++ ex16a.py
@@ -2,31 +2,56 @@ from sys import argv
 
 script, filename = argv
 
+# Establishing Strings
 prompt = ">"
+
+# Establishing Functions
+def startedit(userfilestr):
+    print "What would you like to do with %s?" % userfilestr
+    print "If you would like to erase the file, type ERASE."
+    print "If you would like to append something to the file, type APPEND."
+    print "If you would like to close the file, type CLOSE."
+    print "If you would like to suddenly end this script, press CTRL-C."
+    editmode = raw_input(prompt).upper()
+    return editmode
+
+
+def addtofile(userfile):
+    print "What would you like to write? [END will end appending]"
+    datawrite = raw_input(prompt)
+    if datawrite != "END":
+        userfile.write(datawrite)
+        userfile.write("\n")
+        addtofile(userfile)
+    elif datawrite == "END":
+        pass
+    return "Your information was added to %s!" % filename
+
+def edittype(editmode, userfile):
+    if editmode == "CLOSE":
+        userfile.close()
+        print "File closed."
+    elif editmode == "ERASE":
+        userfile.truncate()
+        print "File erased."
+    elif editmode == "APPEND":
+        status = addtofile(userfile)
+        print status
+    else:
+        print "Thanks for trying!"
+
+
 
 target = open(filename, 'w')
 print "%s is open.\n" % filename
 
-print "What would you like to do with %s?" % filename
-print "If you would like to erase the file, type ERASE."
-print "If you would like to append something to the file, type APPEND."
-print "If you would like to close the file, type CLOSE."
-print "If you would like to end this script, press CTRL-C."
-
-action = raw_input(prompt).upper()
+action = startedit(filename)
 print "You would like to: %s" % action
-
-if action == "CLOSE":
-    target.close()
-    print "File closed."
-elif action == "ERASE":
-    target.truncate()
-    print "File erased."
-elif action == "APPEND":
-    print "Are you sure you want to APPEND stuff to this file?"
-    print "That isn't set up. Please come back later."
+print "Is that correct? [y/n]"
+confirm = raw_input(prompt).upper()
+if confirm == "Y" or "YES":
+    edittype(action, target)
 else:
-    print "Thanks for trying!"
-    target.close()
+    quit()
 
 target.close()
