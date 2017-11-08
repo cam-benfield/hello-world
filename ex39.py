faacb223e1c61f192c5d3ef098fc42f2096b9ee5
Cameryn Rhosyn <camerynrhosyn@gmail.com>
2017-11-08 15:30:55 -0500

    LPTHW

--- ex39.py
+++ ex39.py
@@ -1,18 +1,18 @@
 # create a mapping of state to abbreviation
-states = [
-'Oregon': 'OR',
-'Florida': 'FL',
+states = {
+    'Oregon': 'OR',
+    'Florida': 'FL',
     'California': 'CA',
     'New York': 'NY',
     'Michigan': 'MI'
-]
+}
 
 # creates a basic set of states and some cities in them
-cities = [
+cities = {
     'CA': 'San Francisco',
     'MI': 'Detroit',
     'FL': 'Jacksonville'
-]
+}
 
 # add some more cities
 cities['NY'] = 'New York City'
